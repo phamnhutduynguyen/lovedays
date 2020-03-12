699a403742d7b98a289b3f7f186f054bc4af156b
duynguyen <gianguyengiaa3@gmail.com>
2020-03-12 22:41:17 +0700

    update

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const yourDate = new Date("1999-04-16T00:00:00"),
+const yourDate = new Date("1999-04-16T17:00:00"),
 music = ['ido'];
 
 document.addEventListener('DOMContentLoaded', function(){
@@ -16,7 +16,7 @@ document.addEventListener('DOMContentLoaded', function(){
             rootTime.textContent = `${(hrs>9)?hrs:"0"+hrs}:${(min>9)?min:"0"+min}:${(sec>9)?sec:"0"+sec}`;
       } olock();
       var timer = setInterval(function(){olock()}, 1000);
-      document.querySelector("audio").setAttribute("src", `music/${music[Math.floor(Math.random()*music.length)]}.mp3`);
+      document.querySelector("audio").setAttribute("src", `../music/${music[Math.floor(Math.random()*music.length)]}.mp3`);
 
       document.getElementsByTagName("body")[0].insertAdjacentHTML(
             "beforeend",
